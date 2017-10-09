4128234b53414e6cdc1796a5e6029adb4040d0bd
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-10-09 19:08:45 +0100

    install React Router Dom, breack the bootstrap index into separarte components and import Browserouter and Route from react-router-dom

---
+++ src/components/post_index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class PostIndex extends Component{
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 col-md-10 mx-auto">
+                        <h1>Content here!</h1>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default PostIndex;
