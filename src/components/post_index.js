59f7627ef862979056b42dbe0ac1bb9c417873e2
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-10-10 13:21:07 +0100

    Action Creator Shorcut. Connect the recucer_posts to the posts_index component make use of connet and fetchPosts

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostIndex extends Component{
+
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
+
     render(){
         return(
             <div className="container">
@@ -15,4 +22,4 @@ class PostIndex extends Component{
     }
 }
 
-export default PostIndex;
+export default connect(null, {fetchPosts})(PostIndex);
