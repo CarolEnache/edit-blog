0ec972a27e8ef58f680e61cbef014ed930534d62
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-10-11 16:08:33 +0100

    Deleting a post. Build teh Action Creator using a callback() function as an argument to make use of computed navigation after deliting the post, and export it. Create the delete button and bind it with ede onDeleteClick() function. Import delete post and wire it with connect.

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,7 +1,8 @@
 import React, { Component} from 'react';
 import { connect } from 'react-redux';
-import { fetchPost} from '../actions';
+import { fetchPost, deletePost} from '../actions';
 import { Link} from 'react-router-dom';
+
 
 
 
@@ -11,6 +12,13 @@ class PostsShow extends Component {
     componentDidMount(){
         const { id } = this.props.match.params;
         this.props.fetchPost(id);
+    }
+
+    onDeleteClick(){
+        const { id } = this.props.match.params;
+        this.props.deletePost(id, () =>{
+            this.props.history.push('/');
+        });
     }
 
 
@@ -25,6 +33,13 @@ class PostsShow extends Component {
         return (
             <div>
                  <Link to='/' className='btn btn-primary'>Back to Index</Link>
+                 <button
+                    className='btn btn-danger pull-xs-rigth'
+                    onClick={this.onDeleteClick.bind(this)}
+
+                 >
+                     Delete Post
+                 </button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -37,4 +52,4 @@ function mapStateToProps({ posts }, ownProps){
     return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect( mapStateToProps, {fetchPost})(PostsShow);
+export default connect( mapStateToProps, {fetchPost, deletePost})(PostsShow);
